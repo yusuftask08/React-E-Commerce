5691bff850b703728c2403c586c5f3170232cf11
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-02-02 22:46:03 +0300

    init app

--- client/src/index.js
+++ client/src/index.js
@@ -1,42 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "./reset.css";
-import "antd/dist/antd.css";
-
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-
-// contexts
-import { AuthProvider } from "./contexts/AuthContext";
-import { BasketProvider } from "./contexts/BasketContext";
-
-import { ChakraProvider } from "@chakra-ui/react";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			refetchOnMount: false,
-			refetchOnWindowFocus: false,
-		},
-	},
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-	<QueryClientProvider client={queryClient}>
-		<ChakraProvider>
-			<AuthProvider>
-				<BasketProvider>
-					<App />
-				</BasketProvider>
-			</AuthProvider>
-		</ChakraProvider>
-
-		<ReactQueryDevtools initialIsOpen={false} />
-	</QueryClientProvider>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
